5b8b9f30f2ccb4612f60b7560e49e0504965f160
JI YOUNG <128905066+shinjiyoung04@users.noreply.github.com>
2024-12-18 04:26:29 +0900

    Update Express.js

--- app/api/Express.js
+++ app/api/Express.js
@@ -1,40 +1,13 @@
-// 예시 Express.js API 핸들러
+// lib/dbConnect.js
+import mongoose from 'mongoose'
 
-const express = require('express')
-const router = express.Router()
-const advices = [] // 임시 데이터 저장소
+const dbConnect = async () => {
+  if (mongoose.connection.readyState >= 1) return
 
-// GET 모든 어드바이스 가져오기
-router.get('/advices', (req, res) => {
-  res.json(advices)
-})
+  return mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+}
 
-// POST 새로운 어드바이스 추가하기
-router.post('/advices', (req, res) => {
-  advices.push(req.body)
-  res.status(201).send()
-})
-
-// PUT 어드바이스 수정하기
-router.put('/advices/:id', (req, res) => {
-  const index = advices.findIndex((advice) => advice.id === req.params.id)
-  if (index !== -1) {
-    advices[index] = req.body
-    res.send()
-  } else {
-    res.status(404).send()
-  }
-})
-
-// DELETE 어드바이스 삭제하기
-router.delete('/advices/:id', (req, res) => {
-  const index = advices.findIndex((advice) => advice.id === req.params.id)
-  if (index !== -1) {
-    advices.splice(index, 1)
-    res.send()
-  } else {
-    res.status(404).send()
-  }
-})
-
-module.exports = router
+export default dbConnect
