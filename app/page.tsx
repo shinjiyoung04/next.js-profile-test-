ac107bfd42b35ffc4e4871e7e263a97170cafe81
JI YOUNG <128905066+shinjiyoung04@users.noreply.github.com>
2024-12-18 02:48:55 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import TopicsList from '@/components/TopicsList'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">My project List</h1>
+      <h1 className="text-3xl font-bold">My project List📋</h1>
       <p className="mb-4">Details can be found on the my profile page.</p>
       <TopicsList />
     </div>
