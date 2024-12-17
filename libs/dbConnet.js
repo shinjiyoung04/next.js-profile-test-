b9f39bb56d419b66cbe0d25de71d9b0e875c551e
JI YOUNG <128905066+shinjiyoung04@users.noreply.github.com>
2024-12-18 04:27:50 +0900

    Add files via upload

---
+++ libs/dbConnet.js
@@ -0,0 +1,13 @@
+// lib/dbConnect.ts
+import mongoose from 'mongoose'
+
+const dbConnect = async () => {
+  if (mongoose.connection.readyState >= 1) return
+
+  return mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+}
+
+export default dbConnect
